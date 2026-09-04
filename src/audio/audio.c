5d9d25034c5138b492a1c471e78efa5df2cb9441
Shams Parvez Arka <parvez6826@gmail.com>
2026-09-04 23:18:28 +0600

    imp -> separated audio layer

--- src/audio/audio.c
+++ src/audio/audio.c
@@ -1,6 +1,24 @@
 extern I32 octave_shift;
 
 internal void
+audio_note_lookup_init(void)
+{
+  audio_note_lookup[0]  = (AudioNote){G_InputMap_C,  16.35f, 0.0f, 0.0f, 0, string_lit("NOTE C")};
+  audio_note_lookup[1]  = (AudioNote){G_InputMap_Cs, 17.32f, 0.0f, 0.0f, 0, string_lit("NOTE C#")};
+  audio_note_lookup[2]  = (AudioNote){G_InputMap_D,  18.35f, 0.0f, 0.0f, 0, string_lit("NOTE D")};
+  audio_note_lookup[3]  = (AudioNote){G_InputMap_Ds, 19.45f, 0.0f, 0.0f, 0, string_lit("NOTE D#")};
+  audio_note_lookup[4]  = (AudioNote){G_InputMap_E,  20.60f, 0.0f, 0.0f, 0, string_lit("NOTE E")};
+  audio_note_lookup[5]  = (AudioNote){G_InputMap_F,  21.83f, 0.0f, 0.0f, 0, string_lit("NOTE F")};
+  audio_note_lookup[6]  = (AudioNote){G_InputMap_Fs, 23.12f, 0.0f, 0.0f, 0, string_lit("NOTE F#")};
+  audio_note_lookup[7]  = (AudioNote){G_InputMap_G,  24.50f, 0.0f, 0.0f, 0, string_lit("NOTE G")};
+  audio_note_lookup[8]  = (AudioNote){G_InputMap_Gs, 25.96f, 0.0f, 0.0f, 0, string_lit("NOTE G#")};
+  audio_note_lookup[9]  = (AudioNote){G_InputMap_A,  27.50f, 0.0f, 0.0f, 0, string_lit("NOTE A")};
+  audio_note_lookup[10] = (AudioNote){G_InputMap_As, 29.14f, 0.0f, 0.0f, 0, string_lit("NOTE A#")};
+  audio_note_lookup[11] = (AudioNote){G_InputMap_B,  30.87f, 0.0f, 0.0f, 0, string_lit("NOTE B")};
+  audio_note_lookup[12] = (AudioNote){G_InputMap_Cn, 32.70f, 0.0f, 0.0f, 0, string_lit("NOTE C NEXT OCTAVE")};
+};
+
+internal void
 audio_callback(void *userdata, SDL_AudioStream *stream, I32 additional_amount, I32 total_amount)
 {
   additional_amount /= sizeof(I16);
@@ -22,7 +40,7 @@ audio_callback(void *userdata, SDL_AudioStream *stream, I32 additional_amount, I
       if (note->amplitude > 0.0f || note->active)
       {
         mixed += sinf(note->phase) * note->amplitude * NOTE_VOICE_GAIN;
-        note->phase += note->frequency *octave_mul * PI * 2.0f / AUDIO_SAMPLE_RATE;
+        note->phase += note->frequency * octave_mul * PI * 2.0f / AUDIO_SAMPLE_RATE;
 
         if (note->phase >= PI * 2.0f)
         {
--- src/audio/audio.h
+++ src/audio/audio.h
@@ -19,25 +19,12 @@ struct AudioNote
   F32 phase;
   F32 amplitude;
   B32 active;
+  String note_name;
 };
 
-global AudioNote audio_note_lookup[13] =
-{
-  {G_InputMap_C4,  261.63f, 0.0f, 0.0f, 0},
-  {G_InputMap_Cs4, 277.63f, 0.0f, 0.0f, 0},
-  {G_InputMap_D4,  293.66f, 0.0f, 0.0f, 0},
-  {G_InputMap_Ds4, 311.13f, 0.0f, 0.0f, 0},
-  {G_InputMap_E4,  329.63f, 0.0f, 0.0f, 0},
-  {G_InputMap_F4,  349.23f, 0.0f, 0.0f, 0},
-  {G_InputMap_Fs4, 369.99f, 0.0f, 0.0f, 0},
-  {G_InputMap_G4,  392.00f, 0.0f, 0.0f, 0},
-  {G_InputMap_Gs4, 415.30f, 0.0f, 0.0f, 0},
-  {G_InputMap_A4,  440.00f, 0.0f, 0.0f, 0},
-  {G_InputMap_As4, 466.16f, 0.0f, 0.0f, 0},
-  {G_InputMap_B4,  493.88f, 0.0f, 0.0f, 0},
-  {G_InputMap_C5,  523.25f, 0.0f, 0.0f, 0}
-};
+global AudioNote audio_note_lookup[13] = {};
 
+internal void audio_note_lookup_init(void);
 internal void audio_callback(void *userdata, SDL_AudioStream *stream, I32 additional_amount, I32 total_amount);
 
 #endif
